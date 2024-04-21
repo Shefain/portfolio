47ad46d29d7141df37778c766bbc634f50b7eb98
MaHi <muhammadshefain123@gmail.com>
2024-04-21 22:12:09 +0600

    random commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {  } from 'react'
+
+import './App.scss'
+import Navbar from './Components/Header/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Navbar/index.jsx
@@ -0,0 +1,20 @@
+const Navbar = () => {
+    return (
+     <nav>
+        <div className="Logo">
+            <span>Muhammad</span><span>MaHi</span>
+        </div>
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Portfolio</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    </nav> 
+    );
+}
+ 
+export default Navbar;
