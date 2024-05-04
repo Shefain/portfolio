9da462c4c3b79e161912ec2f3dc1c86216fd0b22
MaHi <muhammadshefain123@gmail.com>
2024-05-04 21:44:52 +0600

    modified readme file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,7 @@
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
-    const Navlinks =[
-        "home",
-        "portfolio",
-        "about",
-        "contact",
-        "resume"
-    ] 
+  const Navlinks = ['home', 'portfolio', 'about', 'contact', 'resume'];
   return (
     <nav className=" flex container m-auto justify-between px-4 h-full items-center">
       <span className="text-4xl underline decoration-wavy decoration-cyan-400 font-semibold">
@@ -15,7 +9,11 @@ const Navbar = () => {
         <span>Hi</span>
       </span>
       <ul className="flex list-none space-x-3 uppercase">
-        {Navlinks.map((item)=> <li key={Math.random()}><NavLink to={`${item == 'home' ? '/': item}`}>{item}</NavLink>  </li>  ) }
+        {Navlinks.map((item) => (
+          <li key={Math.random()}>
+            <NavLink to={`${item == 'home' ? '/' : item}`}>{item}</NavLink>
+          </li>
+        ))}
       </ul>
     </nav>
   );
