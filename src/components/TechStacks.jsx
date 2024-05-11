1cdf0f5c738f528abc06d5131369e9737b58eae5
MaHi <muhammadshefain123@gmail.com>
2024-05-11 19:12:44 +0600

    added assets picturs

---
+++ src/components/TechStacks.jsx
@@ -0,0 +1,16 @@
+const TechStacks = () => {
+    return ( 
+        <section>
+            <div>
+
+            </div>
+            <div>
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+        </section>
+     );
+}
+ 
+export default TechStacks;
